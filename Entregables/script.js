affed657249e4d0e79154c3a4a09847588fb9c03
Luis Alejandro Montenegro Najera <luigiale14@gmail.com>
2026-02-04 09:22:58 -0600

    Show/hide button
    show/hide button contact info

--- Entregables/script.js
+++ Entregables/script.js
@@ -14,6 +14,33 @@ function Hora() {
   return "Buenas noches";
 }
 
-document.getElementById("saludo").textContent = Hora();
+function Saludo(){
+    const saludo1 = document.getElementById("saludo")
+    if(!saludo1) return;
+    const hora = new Date().getHours();
+    let mensaje = "";
+    if (hora >= 6 && hora < 12) mensaje = "Buenos días";
+    else if(hora >= 12 && hora <18) mensaje ="Buenas noches";
+    else mensaje = "Buenas noches"
+    saludo1.textContent = mensaje;
+}
 
+document.addEventListener("DOMContentLoaded", Saludo);
+
+
+
+const btn = document.getElementById("btnToggle");
+const bloque = document.getElementById("bloque");
+
+
+function toggleContenido(idElemento, boton) {
+  const elemento = document.getElementById(idElemento);
+  if (!elemento) return;
+
+  elemento.classList.toggle("oculto");
+
+  boton.textContent = elemento.classList.contains("oculto")
+    ? "Mostrar"
+    : "Ocultar";
+}
 
