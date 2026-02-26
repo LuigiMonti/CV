442e96f3294867bf68b2271038711a2a56a4f906
Luis Alejandro Montenegro Najera <luigiale14@gmail.com>
2026-02-25 20:00:49 -0600

    Service with lists data
    service tu reuse data

---
+++ CV-react/src/Services/services.tsx
@@ -0,0 +1,10 @@
+const dataservices = {
+    skills: ["React", "Typescript", "CSS", "HTML"],
+    Estudios: [
+        {Titulo: "Bachillerato en computación", Establecimiento: "Colegio Elim", Año: "2023- 2024"},
+        {Titulo: "Computer Science", Establecimiento: "UFM", Año: "2025-actualidad"}
+    ],
+    Experiencia: ["Desarrollo web", "APIS", "Github"]
+}
+
+export default dataservices
