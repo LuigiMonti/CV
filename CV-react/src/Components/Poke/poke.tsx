6563bbe2eb2bf1aa5645aa91608cb26243aafba6
Luis Alejandro Montenegro Najera <luigiale14@gmail.com>
2026-03-16 19:36:51 -0600

    Compoments created
    Poke and github components added

---
+++ CV-react/src/Components/Poke/poke.tsx
@@ -0,0 +1,33 @@
+// Components/Pokemons/Pokemons.tsx
+
+import { useState, useEffect } from 'react';
+import pokeService from '../../Services/Pokeapi_service';
+import './Pokemons.css';
+
+function Pokemons() {
+  const [pokemons, setPokemons] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    pokeService.getFavorites()
+      .then(setPokemons)
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) return null;
+
+  return (
+    <div id="easter-egg">
+      {pokemons.map((pokemon: any) => (
+        <img
+          key={pokemon.id}
+          src={pokemon.sprites.front_default}
+          alt={pokemon.name}
+          title={pokemon.name}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Pokemons;
